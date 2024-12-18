0f488a735c0c7607fa060a39409a719fb4c1e359
Óscar Jiménez <terceroonceoscar@gmail.com>
2024-12-18 23:40:02 +0100

    feat: format files

--- src/App.js
+++ src/App.js
@@ -12,134 +12,124 @@ import Snow from './assets/snow.png';
 import Thunderstorm from './assets/thunderstorm.png';
 
 const LocationNotFound = () => {
-    return (
-        <div className="not-found">
-            <img
-                src={LocationError}
-                alt="Location not found"
-            />
-            <p>Oops! Invalid location :/</p>
-        </div>
-    );
+  return (
+    <div className="not-found">
+      <img src={LocationError} alt="Location not found" />
+      <p>Oops! Invalid location :/</p>
+    </div>
+  );
 };
 
 function App() {
-    const [location, setLocation] = useState('');
-    const [weatherData, setWeatherData] = useState({});
-    const [weatherPicture, setWeatherPicture] = useState('');
-    const [isError, setIsError] = useState(false);
+  const [location, setLocation] = useState('');
+  const [weatherData, setWeatherData] = useState({});
+  const [weatherPicture, setWeatherPicture] = useState('');
+  const [isError, setIsError] = useState(false);
 
-    const handleChangeLocation = (event) => {
-        setLocation(event.target.value);
-    };
+  const handleChangeLocation = (event) => {
+    setLocation(event.target.value);
+  };
 
-    const handleClick = async () => {
-        const response = await weatherAPI(location);
+  const handleClick = async () => {
+    const response = await weatherAPI(location);
 
-        if (response.cod !== 200) {
-            setIsError(true);
-            return;
-        }
+    if (response.cod !== 200) {
+      setIsError(true);
+      return;
+    }
 
-        setIsError(false);
-        setWeatherData(response);
+    setIsError(false);
+    setWeatherData(response);
 
-        switch (response.weather[0].main) {
-            case 'Clear':
-                setWeatherPicture(Clear);
-                break;
+    switch (response.weather[0].main) {
+      case 'Clear':
+        setWeatherPicture(Clear);
+        break;
 
-            case 'Clouds':
-                setWeatherPicture(Clouds);
-                break;
+      case 'Clouds':
+        setWeatherPicture(Clouds);
+        break;
 
-            case 'Drizzle':
-                setWeatherPicture(Drizzle);
-                break;
+      case 'Drizzle':
+        setWeatherPicture(Drizzle);
+        break;
 
-            case 'Fog':
-                setWeatherPicture(Fog);
-                break;
+      case 'Fog':
+        setWeatherPicture(Fog);
+        break;
 
-            case 'Rain':
-                setWeatherPicture(Rain);
-                break;
+      case 'Rain':
+        setWeatherPicture(Rain);
+        break;
 
-            case 'Snow':
-                setWeatherPicture(Snow);
-                break;
+      case 'Snow':
+        setWeatherPicture(Snow);
+        break;
 
-            case 'Thunderstorm':
-                setWeatherPicture(Thunderstorm);
-                break;
+      case 'Thunderstorm':
+        setWeatherPicture(Thunderstorm);
+        break;
 
-            default:
-                setWeatherPicture('');
-        }
-    };
+      default:
+        setWeatherPicture('');
+    }
+  };
 
-    return (
-        <div
-            className={`container ${
-                isError || weatherData?.id ? 'container-open' : ''
-            }`}
-        >
-            <div className="search-box">
-                <i className="fa-solid fa-location-dot"></i>
-                <input
-                    type="text"
-                    placeholder="Enter your location"
-                    value={location}
-                    onChange={handleChangeLocation}
-                />
-                <button
-                    className="fa-solid fa-magnifying-glass"
-                    onClick={handleClick}
-                    disabled={location.length < 3}
-                ></button>
+  return (
+    <div
+      className={`container ${
+        isError || weatherData?.id ? 'container-open' : ''
+      }`}
+    >
+      <div className="search-box">
+        <i className="fa-solid fa-location-dot"></i>
+        <input
+          type="text"
+          placeholder="Enter your location"
+          value={location}
+          onChange={handleChangeLocation}
+        />
+        <button
+          className="fa-solid fa-magnifying-glass"
+          onClick={handleClick}
+          disabled={location.length < 3}
+        ></button>
+      </div>
+
+      {isError && <LocationNotFound />}
+
+      {!isError && weatherData?.id && (
+        <>
+          <div className="weather-box">
+            <img src={weatherPicture} alt="Weather Box" />
+            <p className="temperature">
+              {Math.round(weatherData.main.temp)}
+              <span>°C</span>
+            </p>
+            <p className="description">{weatherData.weather[0].description}</p>
+          </div>
+
+          <div className="weather-details">
+            <div className="humidity">
+              <i className="fa-solid fa-water"></i>
+              <div className="text">
+                <span>{weatherData.main.humidity}%</span>
+                <p>Humidity</p>
+              </div>
             </div>
 
-            {isError && <LocationNotFound />}
-
-            {!isError && weatherData?.id && (
-                <>
-                    <div className="weather-box">
-                        <img
-                            src={weatherPicture}
-                            alt="Weather Box"
-                        />
-                        <p className="temperature">
-                            {Math.round(weatherData.main.temp)}
-                            <span>°C</span>
-                        </p>
-                        <p className="description">
-                            {weatherData.weather[0].description}
-                        </p>
-                    </div>
-
-                    <div className="weather-details">
-                        <div className="humidity">
-                            <i className="fa-solid fa-water"></i>
-                            <div className="text">
-                                <span>{weatherData.main.humidity}%</span>
-                                <p>Humidity</p>
-                            </div>
-                        </div>
-
-                        <div className="wind">
-                            <i className="fa-solid fa-wind"></i>
-                            <div className="text">
-                                <span>
-                                    {Math.round(weatherData.wind.speed)} Km/h
-                                </span>
-                                <p>Wind Speed</p>
-                            </div>
-                        </div>
-                    </div>
-                </>
-            )}
-        </div>
-    );
+            <div className="wind">
+              <i className="fa-solid fa-wind"></i>
+              <div className="text">
+                <span>{Math.round(weatherData.wind.speed)} Km/h</span>
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,9 @@
 const API_KEY = '1265140e7dc5046b767bdafd08bcccdd';
 
 async function weatherAPI(city) {
-    return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
-        .then(response => response.json());
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+  ).then((response) => response.json());
 }
 
 export default weatherAPI;
